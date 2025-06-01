9ae19adabfffd4589760a39d233a43fbf6447dc4
Santino-Argensys <santino.spingola@argensys.com.ar>
2025-06-01 19:20:27 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require('./db');
+const path = require('path');
 
 const app = express();
 app.use(cors());
